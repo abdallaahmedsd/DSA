200edc6959a7f28fcc9c831d14acde096dc6f66e
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2025-06-15 05:22:49 +0300

    Add BinaryTreeNode class and sample program
    Introduced a new `BinaryTreeNode<T>` class in the `DSA.Tree` project
    with properties for `Data`, `Left`, and `Right`. Implemented traversal
    methods (`Bfs`, `DfsPreOrder`, `DfsPostOrder`, `DfsInOrder`) and a
    `ToString` override.
    
    Created a `.NET 8.0` project (`DSA.Tree.csproj`) with nullable
    reference types enabled and added a solution file (`DSA.sln`) for
    project organization.
    
    Added a sample program (`Program.cs`) to demonstrate binary tree
    construction and traversal methods with console output.

---
+++ DSA.Tree/BinaryTreeNode.cs
@@ -0,0 +1,101 @@
+﻿
+namespace DSA.Tree
+{
+    public  class BinaryTreeNode<T>
+    {
+        public T Data { get; set; }
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
+
+        public BinaryTreeNode(T value)
+        {
+            Data = value;
+            Left = null;
+            Right = null;
+        }
+
+        public void Bfs()
+        {
+            Bfs(this);
+        }
+
+        public void DfsPreOrder()
+        {
+            DfsPreOrder(this);
+        }
+
+        public void DfsPostOrder()
+        {
+            DfsPostOrder(this);
+        }
+
+        public void DfsInOrder()
+        {
+            DfsInOrder(this);
+        }
+
+        private void DfsInOrder(BinaryTreeNode<T> node)
+        {
+            if (node == null)
+                return;
+
+            DfsInOrder(node.Left);
+            Console.Write(node.Data + " ");
+            DfsInOrder(node.Right);
+        }
+
+        private void DfsPreOrder(BinaryTreeNode<T> node)
+        {
+            if (node == null)
+                return;
+
+            Console.Write(node.Data + " ");
+            // Visit left child
+            DfsPreOrder(node.Left);
+            // Visit right child
+            DfsPreOrder(node.Right);
+        }
+
+        private void DfsPostOrder(BinaryTreeNode<T> node)
+        {
+            if (node == null)
+                return;
+
+            // Visit left child
+            DfsPostOrder(node.Left);
+
+            // Visit right child
+            DfsPostOrder(node.Right);
+
+            Console.Write(node.Data + " ");
+        }
+
+        private void Bfs(BinaryTreeNode<T> node)
+        {
+            var queue = new Queue<BinaryTreeNode<T>>();
+            queue.Enqueue(node);
+
+            while(queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                Console.Write(current.Data + " ");
+                // Enqueue left child
+                if (current.Left != null)
+                {
+                    queue.Enqueue(current.Left);
+                }
+                // Enqueue right child
+                if (current.Right != null)
+                {
+                    queue.Enqueue(current.Right);
+                }
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ DSA.Tree/Program.cs
@@ -0,0 +1,28 @@
+﻿using DSA.Tree;
+
+/*
+        1
+       / \
+      2   3
+     / \ / \
+    4  5 6  7
+ */
+
+var root = new BinaryTreeNode<int>(1);
+root.Left = new BinaryTreeNode<int>(2);
+root.Right = new BinaryTreeNode<int>(3);
+root.Left.Left = new BinaryTreeNode<int>(4);
+root.Left.Right = new BinaryTreeNode<int>(5);
+root.Right.Left = new BinaryTreeNode<int>(6);
+root.Right.Right = new BinaryTreeNode<int>(7);
+
+Console.WriteLine("BFS Pre Order Traversal:");
+root.DfsPreOrder();
+
+Console.WriteLine("\n\nBFS Post Order Traversal:");
+root.DfsPostOrder();
+
+Console.WriteLine("\n\nBFS In Order Traversal: ");
+root.DfsInOrder();
+
+Console.ReadKey();
